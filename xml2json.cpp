5d42c9ae6635a9c43b88a7542f4971b162949828
jyjy1229 <kimjy18@kaist.ac.kr>
2021-01-08 11:33:30 +0900

    Make json file

--- xml2json.cpp
+++ xml2json.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <iostream>
 #include <string>
 #include <stack>
 
@@ -8,23 +9,31 @@ using namespace std;
 stack <string> tags;
 
 int main(){
-    FILE *xml = NULL;
+    FILE *xml = NULL, *json = NULL;
     xml = fopen("network.xml", "r");
+    json = fopen("network.json", "w");
 
-    // Error check
     if(xml == NULL){
         printf("Cannot open file");
         return 0;
     }
+
     // Read xml file
-    char sentence[1023], tag[100];
+    char sentence[512], tag[100];
+    bool first = true;
+    int i = 0, j = 0, depth = 0;
     while(!feof(xml)){
-        fgets(sentence, 1023, xml);
-        // printf("%s", sentence);
+        memset(tag, 0, sizeof(tag));
+        memset(sentence, 0, sizeof(sentence));
+        i = 0; j = 0;
+
+        fgets(sentence, sizeof(sentence), xml);
+        if(first){
+            first = false;
+            continue;
+        }
         
         // Get tag
-        int i = 0, j = 0;
-        memset(tag, 0, sizeof(tag));
         while(sentence[i] == ' ' || sentence[i] == '<' || sentence[i] == '/'){
             i++;
         }
@@ -33,9 +42,35 @@ int main(){
             i++;
             j++;
         }
-        printf("%s\n", tag);
+
+        if(!tags.empty() && tags.top().compare(string(tag))){
+            fprintf(json, "}\n");
+            tags.pop();
+            depth--;
+            continue;
+        }
+        else{
+            fprintf(json, "{\n");
+            tags.push(string(tag));
+            depth++;
+            for(int i = 0;i < depth;i++){
+                fprintf(json, "\t");
+            }
+            fprintf(json, "\"%s\":", tag);
+        }
+        
+        // Split space
+        while(sentence[i] != '>'){
+            i++;
+        }
+        if(sentence[i-1] == '/'){
+            fprintf(json, "}\n");
+            tags.pop();
+            depth--;
+        }
     }
 
     fclose(xml);
+    fclose(json);
     return 0;
 }
